7910a295fc7e9d8da5fc441b9acdac6125e5b419
myhuon <myhuon@naver.com>
2021-11-25 12:13:02 +0900

    make UI

---
+++ app/src/main/java/ddwucom/mobile/searchrestaurant/DetailViewActivity.java
@@ -0,0 +1,14 @@
+package ddwucom.mobile.searchrestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view);
+    }
+}
